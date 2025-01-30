f49ba1f1913d33106de064b47a958bc7b6cfb8bf
Seya <31957516+sglkc@users.noreply.github.com>
2025-01-30 23:45:26 +0700

    feat: add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
+// TODO: env database
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'kuasar',
+      entities: [User],
+      synchronize: process.env.NODE_ENV !== 'production' // TODO: @nestjs/config?
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
