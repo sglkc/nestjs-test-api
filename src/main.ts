7a04c84e3e37308e5e7e0c13e48cf4556a453cce
Seya <31957516+sglkc@users.noreply.github.com>
2025-01-31 16:56:21 +0700

    feat(main): add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth Test API')
+    .setDescription('Basic auth with NestJS')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, documentFactory)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 // eslint-disable-next-line
