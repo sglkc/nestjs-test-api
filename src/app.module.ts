d88712e0e048ead742d98cc0acffb3466b7708da
Seya <31957516+sglkc@users.noreply.github.com>
2025-01-31 17:00:30 +0700

    chore: delete unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AppDataSourceOptions } from 'typeorm.config';
 
 @Module({
   imports: [TypeOrmModule.forRoot(AppDataSourceOptions), UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
