21c6fa77d11d565ae92225a1cdb00843fecd576a
Seya <31957516+sglkc@users.noreply.github.com>
2025-02-04 18:12:31 +0700

    feat: add rate limit guard
    build(pnpm): add @nestjs/throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AppDataSourceOptions } from 'typeorm.config';
@@ -7,10 +9,20 @@ import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 10_000,
+        limit: 10,
+        blockDuration: 5_000,
+      }
+    ]),
     TypeOrmModule.forRoot(AppDataSourceOptions),
     UsersModule,
     AuthModule,
     HealthModule,
   ],
+  providers: [
+    { provide: APP_GUARD, useClass: ThrottlerGuard }
+  ]
 })
 export class AppModule {}
